b50c393aa24954dc9300bfe5b80c8576e377d903
ArcaneMisery <vova.kudinov.2017@inbox.ru>
2023-06-04 14:53:18 +0300

    Fixed token.

--- frontend/src/app/core.module/interceptors/auth.interceptor.ts
+++ frontend/src/app/core.module/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable, from, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, filter, from, switchMap, take, throwError } from 'rxjs';
 import { ManageService } from 'src/app/services/manage.service';
 
 @Injectable()
@@ -9,6 +9,9 @@ export class authInterceptor { }
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  
   constructor(
     private cookieService: CookieService,
     private manageService: ManageService
@@ -17,16 +20,46 @@ export class AuthInterceptor implements HttpInterceptor {
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.cookieService.get("accesstoken");
     const role = this.cookieService.get("role");
-    if (!authToken) {
-      return next.handle(request);
-    }
-      request = request.clone({
-        setHeaders: {
-          "Authorization": "Bearer " + authToken,
-          "Role": role
+    if (authToken) {
+      request = this.addTokenHeader(request, authToken, role);
+      return next.handle(request).pipe(catchError(error => {
+        if (error instanceof HttpErrorResponse && !request.url.includes('user/sign_in') && error.status === 403) {
+          return this.handle403Error(request, next);
         }
-      });
-      return next.handle(request);
+
+        return throwError(error);
+      }));
+    }
+    return next.handle(request);
+  }
+
+  private handle403Error(request: HttpRequest<any>, next: HttpHandler) {
+    const role = this.cookieService.get("role");
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+      const token = this.cookieService.get("jwt");
+
+      if (token) {
+        return this.manageService.refresh(token).pipe(
+          switchMap((data: any) => {
+            this.isRefreshing = false;
+            this.refreshTokenSubject.next(data.accessToken);
+            return next.handle(this.addTokenHeader(request, data.accessToken, role));
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+            return throwError(err);
+          })
+        );
+      }
+    }
+
+    return this.refreshTokenSubject.pipe(
+      filter(token => token !== null),
+      take(1),
+      switchMap((token) => next.handle(this.addTokenHeader(request, token, role)))
+    );
       
 
     // if (!this.manageService.token?.isAlive) {
@@ -54,5 +87,14 @@ export class AuthInterceptor implements HttpInterceptor {
     //   });
     //   return next.handle(request);
     // }
+  }
+
+  private addTokenHeader(request: HttpRequest<any>, token: string, role: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        "Authorization": "Bearer " + token,
+        "Role": role
+      }
+    });
   }
 }
